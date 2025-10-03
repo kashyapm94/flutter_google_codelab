ccd7cac68c711441ae8ca7e66c11f60ce1a325ed
kashyapm94 <kashyapmaheshwari20031994@gmail.com>
2025-10-03 15:24:31 +0200

    make the app prettier

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,92 @@ void main() {
   runApp(MyApp());
 }
 
+// MyApp class extends StatelessWidget. Widgets are the elements from which you build every Flutter app.
+// MyApp is the class declaration and const MyApp() is the constructor.
+// const keyword means the object is created at compile time (instead of runtime)
 class MyApp extends StatelessWidget {
+  // Passes the key parameter up to the StatelessWidget constructor.
   const MyApp({super.key});
 
+  // As you can see, even the app itself is a widget.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) => MyAppState(), // app wide state
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Namer App', // app name
         theme: ThemeData(
+          //visual style
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: MyHomePage(),
+        home: MyHomePage(), // home widget, the starting point of the app
       ),
     );
   }
 }
 
+// MyAppState defines the data the app needs to function.
+// The state class extends ChangeNotifier, which means that it can notify others about its own changes. For example, if the current word pair changes, some widgets in the app need to know.
+// The state is created and provided to the whole app using a ChangeNotifierProvider (see code above in MyApp). This allows any widget in the app to get hold of the state.
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners(); // notify the widgets that depend on this state
+  }
 }
 
+// Every widget defines a build() method that's automatically called every time the widget's circumstances change so that the widget is always up to date.
 class MyHomePage extends StatelessWidget {
+  // Every build method must return a widget or (more typically) a nested tree of widgets.
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current; // get the current state
 
     return Scaffold(
-      body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10), // create some space between the two cards
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  // Once assigned, the value cannot be changed. (Immutable after construction)
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(
+      context,
+    ); // get the current theme from the ThemeData in MyApp
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Card(
+      elevation: 5,
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
